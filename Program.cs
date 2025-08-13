d00b7c72bdda70d7e006baa85961596cbab882d4
Abhishek_7 <abhisheknshetty656@gmail.com>
2025-08-13 10:28:46 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class ReverseNumber
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        int reversed = 0;
+
+        while (number != 0)
+        {
+            int digit = number % 10;           
+            reversed = reversed * 10 + digit; 
+            number /= 10;                      
+        }
+
+        Console.WriteLine("Reversed number: " + reversed);
+    }
+}
