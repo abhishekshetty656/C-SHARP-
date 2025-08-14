12239943df6229932e682bb8bff029d61fc9f203
Abhishek_7 <abhisheknshetty656@gmail.com>
2025-08-14 14:49:20 +0530

    Add files via upload

---
+++ Fibonacci .cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Fibonacciseries
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of terms in the Fibonacci series:");
+        if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+        {
+            Console.WriteLine("Fibonacci Series:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid positive integer.");
+        }
+    }
+    static int Fibonacci(int term)
+    {
+        if (term <= 1)
+            return term;
+        return Fibonacci(term - 1) + Fibonacci(term - 2);
+    }
+}
