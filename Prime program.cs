d27b568e8f9be7bde938b1b99ff15c8bfe059f22
Abhishek_7 <abhisheknshetty656@gmail.com>
2025-08-14 13:48:34 +0530

    Add files via upload

---
+++ Prime program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class primenum
+    {
+    static void Main(string[] args)
+    {
+        int n = 100;
+        bool isPrime;
+        for (int i = 2; i <= n; i++)
+        {
+            isPrime = true;
+            for (int j = 2; j <= Math.Sqrt(i); j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
