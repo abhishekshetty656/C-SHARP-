4e39cd77eae1055b779189ecd56bfdd804423149
Abhishek_7 <abhisheknshetty656@gmail.com>
2025-09-18 14:16:08 +0530

    Add files via upload

---
+++ CALCUALTING AGE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter DOB (yyyy-mm-dd): ");
+        DateTime dob = DateTime.Parse(Console.ReadLine());
+        int age = DateTime.Now.Year - dob.Year;
+
+        if (DateTime.Now.DayOfYear < dob.DayOfYear)
+            age--;
+
+        Console.WriteLine($"You are {age} years old.");
+    }
+}
+
