68175171f36bf3e3150599a975128ea73607d275
Abhishek_7 <abhisheknshetty656@gmail.com>
2025-09-18 14:47:27 +0530

    Add files via upload

---
+++ REVERSING NUM C SHARP.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter an integer: ");
+        if (int.TryParse(Console.ReadLine(), out int num))
+        {
+            int rev = 0;
+            while (num != 0)
+            {
+                rev = rev * 10 + num % 10;
+                num /= 10;
+            }
+            Console.WriteLine("Reversed Number: " + rev);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input.");
+        }
+    }
+}
