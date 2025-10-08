4acfd1e62143b2867f8f333c67a55f3ddad55c44
Abhishek_7 <abhisheknshetty656@gmail.com>
2025-10-08 13:15:51 +0530

    Add files via upload

---
+++ Bank account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class BankAccount
+{
+    public double Balance = 0;
+    public void Deposit(double amount)
+    {
+        Balance += amount;
+        Console.WriteLine("Deposited: " + amount);
+    }
+    public void Withdraw(double amount)
+    {
+        if (amount <= Balance)
+        {
+            Balance -= amount;
+            Console.WriteLine("Withdrawn: " + amount);
+        }
+        else
+            Console.WriteLine("Insufficient Balance");
+    }
+}
+class SavingsAccount : BankAccount
+{
+    public void AddInterest(double rate)
+    {
+        double interest = Balance * rate / 100;
+        Balance += interest;
+        Console.WriteLine("Interest Added: " + interest);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        SavingsAccount sa = new SavingsAccount();
+        sa.Deposit(1000);
+        sa.AddInterest(5);
+        sa.Withdraw(500);
+    }
+}
