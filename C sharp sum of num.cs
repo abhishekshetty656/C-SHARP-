beeb3488ad70e1b86fd32e68507496fb7a48520f
Abhishek_7 <abhisheknshetty656@gmail.com>
2025-10-08 13:14:07 +0530

    Add files via upload

---
+++ C sharp sum of num.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+        while (num > 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+        Console.WriteLine("Sum of Digits: " + sum);
+    }
+}
