925f60d496e295a47be86f643607f8e767c89eaa
Abhishek_7 <abhisheknshetty656@gmail.com>
2025-10-08 13:05:42 +0530

    Add files via upload

---
+++ C sharp oddeven program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class NumberCheck
+{
+    int num;
+    public void Read()
+    {
+        Console.Write("Enter a number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+    }
+    public void Display()
+    {
+        if (num % 2 == 0)
+            Console.WriteLine(num + " is Even.");
+        else
+            Console.WriteLine(num + " is Odd.");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        NumberCheck n = new NumberCheck();
+        n.Read();
+        n.Display();
+    }
+}
