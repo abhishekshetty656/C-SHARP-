bc299926ab25bed3a11ea6c573b00106e2e8628a
Abhishek_7 <abhisheknshetty656@gmail.com>
2025-10-08 13:11:11 +0530

    Add files via upload

---
+++ Classobject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Person
+{
+    public string Name;
+    public int Age;
+    public void Read()
+    {
+        Console.Write("Enter Name: ");
+        Name = Console.ReadLine();
+        Console.Write("Enter Age: ");
+        Age = Convert.ToInt32(Console.ReadLine());
+    }
+    public void Display()
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Age: " + Age);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Person p = new Person();
+        p.Read();
+        p.Display();
+    }
+}
