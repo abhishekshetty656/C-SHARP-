c1c04162dcd47dd126a5b7a66fddaa0484e4c0d8
Abhishek_7 <abhisheknshetty656@gmail.com>
2025-10-08 13:08:59 +0530

    Add files via upload

---
+++ C sharp check number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num > 0)
+            Console.WriteLine("Positive");
+        else if (num < 0)
+            Console.WriteLine("Negative");
+        else
+            Console.WriteLine("Zero");
+    }
+}
